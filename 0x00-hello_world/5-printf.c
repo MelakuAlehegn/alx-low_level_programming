f5e8be76ca798a1360091557c354b355c7c35ac3
Melaku Alehegn <melakualehegn34@gmail.com>
2021-08-28 18:33:20 +0300

    5-printf.c added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+include<stdio.h>
+/**
+ * main is where the program starts
+ * return value must be zero
+ */
+int main(void){
+printf("\n"with proper grammer, but the outcome is a piece of art,");
+return (0);
+}
