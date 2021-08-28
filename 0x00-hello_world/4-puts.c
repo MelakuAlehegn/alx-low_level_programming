f9bc3713c30a99999dc0b33dbaf3d0dbcb693928
Melaku Alehegn <melakualehegn34@gmail.com>
2021-08-28 14:16:33 +0300

    puts.c added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  *
  *return: Always return zero 
  */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
