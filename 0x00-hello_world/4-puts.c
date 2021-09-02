aad7df154cfab31382c42c82bf4419e6a18effb5
MelakuAlehegn <melakualehegn34@gmail.com>
2021-09-02 01:14:52 -0700

    merge added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /** 
- *main: starting point
- *
- *return: Always return zero 
- */
-int main(void)
-{
+*main - starting point
+*
+*Return: Always zero 
+*/
+int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
