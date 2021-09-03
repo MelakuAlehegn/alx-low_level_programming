b8accfb3262fe1a0fe03a258e6a928015243c81f
MelakuAlehegn <melakualehegn34@gmail.com>
2021-09-03 14:53:34 -0700

    3-print_numbers.c updated

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_numbers - Print numbers from 0 to 9
+ *Return: no
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+		
+		if (n = 50 || n =52){
+		  continue;
+		}
+	}
+	_putchar('\n');
+}
