eb61bdc28d3426b11797dab2f7f7b986acd7216f
MelakuAlehegn <melakualehegn34@gmail.com>
2021-09-03 06:14:43 -0700

    1-isdigit added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isupper - Checks for uppercase character
+ *@c: Input char
+ *Return: (1) if c is uppercase and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
