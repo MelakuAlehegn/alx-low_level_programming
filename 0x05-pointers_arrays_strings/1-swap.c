858c84b417a158f55f64855d98c97557ea5f0bcf
melakualehegn <melakualehegn34@gmail.com>
2021-09-07 03:26:10 -0700

    1-swap.c added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int - Swaps two integers
+ *@a: Input
+ *@b: Input
+ *Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+*a = temp;
+temp = *b;
+}
