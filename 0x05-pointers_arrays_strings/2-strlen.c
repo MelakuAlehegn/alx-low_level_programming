2b9c1b2d1df9a0d97334432beae4bcc3e99b5836
melakualehegn <melakualehegn34@gmail.com>
2021-09-07 08:21:38 -0700

    3-puts.c added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+/**
+ *_strlen - Returns the length of a string
+ *@str: Input string
+ *Return: Length of string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
