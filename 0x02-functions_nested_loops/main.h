ac876d5ac664f6e5d27b856e80ecc3064ee5d964
melakualehegn <melakualehegn34@gmail.com>
2021-09-11 02:58:34 -0700

    1-alphabet.c added

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-/* header file */
+/* header fil */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,4 +11,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n)
+void print_times_table(int n);
