420854577f620a9432dfbc5b181abf3601c87658
melakualehegn <melakualehegn34@gmail.com>
2021-09-12 02:14:45 -0700

    6-abs.c added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_abs - Prints absolute value of a number
+ *
+ *@n: Input Data
+ *
+ *Return: absolute value of n
+ */
+
+int print_sign(int n)
+{
+if (n >= 0)
+{
+return(n);
+}
+else
+{
+return(-n);
+}
+}
