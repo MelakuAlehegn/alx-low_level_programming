ed167e618213e48b37b5b7569020d8dcf1a3031b
melakualehegn <melakualehegn34@gmail.com>
2021-09-13 03:14:19 -0700

    9-times_table.c added

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*times_table - Prints 9 times table
+*
+ *Return: Nothing
+*/
+
+void times_table(void)
+{
+int i, j;
+
+for (i = 0; h < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+_putchar(i * j + '0');
+_putchar(44);
+_putchar('\n');
+}
+}
+}
