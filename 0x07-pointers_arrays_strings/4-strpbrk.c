a1d0e45a32c115ac8e8ef34444d0dc09c2f1c8b6
melakualehegn <melakualehegn34@gmail.com>
2021-09-13 15:02:01 -0700

    4-strpbrk.c update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,14 +14,16 @@ char *_strpbrk(char *s, char *accept)
 char *c;
 char *null = NULL;
 int i;
+
 for (i = 0; i < s[i]; i++)
 {
 int j;
+
 for (j = 0; j < accept[j]; j++)
 {
 if (s[i] == accept[j])
 {
-c = i + j;
+*c = i + j;
 return (c);
 }
 }
