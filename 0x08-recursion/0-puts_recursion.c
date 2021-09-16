910efe44baaf70e7b316bc13d75c0288857df255
melakualehegn <melakualehegn34@gmail.com>
2021-09-16 09:57:47 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-if (*(s + 1) != '\0')
-{
-putchar(*s);
-_puts_recursion(s + 1);
-}
-else
-putchar('\n');
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
