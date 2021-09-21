a8eb106378b36f93103dbe491a5831de0ac1f882
melakualehegn <melakualehegn34@gmail.com>
2021-09-21 02:31:41 -0700

    3-mul.c updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,24 @@
+#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * main - prints product of two numbers
+ *main - prints product of two numbers
  *@argc: number of argument
  *@argv: Argumnet vector
- *return: 1 if function works 0 for error
+ *Return: 1 if function works 0 for error
  */
 int main(int argc, char *argv[])
 {
-int num1,num2,mul;
-if (argc !=3)
+int num1, num2, mul;
+if (argc != 3)
 {
 printf("Error\n");
-return(0);
+return (1);
 }
-else
-{
-num1 = atio(argv[1]);
-num2 = atio(argv[2]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
 mul = num1 * num2;
 printf("%d\n", mul);
-return(0);
-}
+return (0);
 }
