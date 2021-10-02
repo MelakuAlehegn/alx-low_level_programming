c2bae36961667fa0be5964c2dc62f2f786ae4fe7
melakualehegn <melakualehegn34@gmail.com>
2021-10-02 02:44:37 -0700

    error fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,21 +3,20 @@
 /**
  *main - starting point
  *
- *Return: Aways (0)
+ *Return: Always (0)
  */
 int main(void)
 {
-int oldnum = 1;
-int curnum = 1;
-int nextnum;
-
-printf("%d\n", curnum);
-while (curnum < 50)
+int c = 1;
+int a = 1;
+int b = 1;
+   
+while(c < 50)
 {
-printf("%d\n", curnum);
-nextnum = curnum + oldnum;
-oldnum = curnum;
-curnum = nextnum;
+printf("%d\n",c);
+a = b;
+b = c;
+c = a + b;
 }
 return (0);
 }
