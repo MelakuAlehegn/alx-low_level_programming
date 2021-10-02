524581f2b55add079803559bbad415edb9faa5e0
melakualehegn <melakualehegn34@gmail.com>
2021-10-02 00:55:19 -0700

    error fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,34 @@
 
 void times_table(void)
 {
-int i, j;
+	int i, j, result;
 
-for (i = 0; h < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-_putchar(i * j + '0');
-_putchar(44);
-_putchar('\n');
-}
-}
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (result >  9)
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else if (j != 0)
+			{
+				_putchar(32);
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(j + '0');
+			}
+
+			if (j != 9)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+		_putchar('\n');
+	}
 }
