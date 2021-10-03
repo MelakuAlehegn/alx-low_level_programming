1c2066ac93c5ec082e25eb021040661c82bb3419
MelakuAlehegn <melakualehegn34@gmail.com>
2021-10-03 02:44:31 -0700

    5-more_numbers.c fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,15 @@
 void more_numbers(void)
 {
 int n, m;
+
 for (m = 0; m < 10; m++)
 {
 for (n = 0; n <= 14; n++)
 {
 if (n > 9)
 {
-_putchar(n / 10 + '0' );
- }
+_putchar(n / 10 + '0');
+}
 _putchar(n % 10 + '0');
 }
 _putchar('\n');
