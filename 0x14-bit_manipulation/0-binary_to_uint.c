8e2a3eea56bbe26f23e8acd0b205ac49560ada97
MelakuAlehegn <melakualehegn34@gmail.com>
2021-10-16 02:55:36 -0700

    1-print_binary.c added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,13 +2,12 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- *
- *@b: char
- * 
- *Return: the converted number, or 0 if string b that is no 0 or 1, b is NULL
- *
- */
+* binary_to_uint - converts a binary number to an unsigned int
+*
+*@b: char 
+*Return: the converted number, or 0 if string b that is no 0 or 1, b is NULL
+*
+*/
 
 unsigned int binary_to_uint(const char *b)
 {
@@ -17,13 +16,11 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		dec = 2 * dec + (b[i] - '0');
 	}
-
 	return (dec);
 }
