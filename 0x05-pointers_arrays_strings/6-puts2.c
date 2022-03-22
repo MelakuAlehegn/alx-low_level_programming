f33294077a00c85aebc292d3ba91f4eac79e568c
melakualehgn <melakualehegn34@gmail.com>
2022-03-22 12:27:30 -0700

    puts fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - prints every other charactor 
+ * puts2 - prints every other charactor
  * @str: input charactor
  * Return: every  other char
  */
@@ -9,15 +9,15 @@ void puts2(char *str)
 {
 	int i;
 
-	for(i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		int n;
+
 		n = i % 2;
-		
-		if(n == 0)
+		if (n == 0)
 		{
 			_putchar(str[i]);
 		}
 	}
-		_putchar(10);	
+		_putchar(10);
 }
