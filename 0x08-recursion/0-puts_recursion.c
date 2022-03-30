c5b6e27cfeebfda3bed57ebdf0bc89ca645665dd
melakualehgn <melakualehegn34@gmail.com>
2022-03-30 03:35:43 -0700

    print reverse

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,16 +4,15 @@
  *_puts_recursion - prints a string
  *
  *@s: string to be printed
- *Return: 0
+ *Return: nothing
  **/
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
-		return;
+		_putchar('\n');
 	}
-	putchar(*s);
-	puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(++s);
 }
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,1 @@
-this folder is created for 0x08-recursion project
+This folder is created for 0x08-recursion project of alx low level programming
