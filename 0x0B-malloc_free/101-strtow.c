98f3fb0d01166323840316aac08ecf768be2ce72
melakualehgn <melakualehegn34@gmail.com>
2022-04-06 08:28:13 -0700

    Advanced Tasks

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,50 +1,48 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * strtow - A function that splits a string into words
- * @str: An input pointer of the string to split
- * Return: Apointer to concatened strings or NULL if it str is NULL
+ **argstostr - Concatenates all the arguments of your program
+ *@ac: Argument count
+ *@av: Argument vactor
+ *Return: (NULL) if ac == 0 or av == NULL
+ *Returns a pointer to a new string, or NULL if it fails
  */
-char **strtow(char *str)
-{
-	char **array;
-	int i = 0, j, m, k = 0, len = 0, count = 0;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	for (; str[i]; i++)
+char *argstostr(int ac, char **av)
+{
+	char *x, *newstr;
+	int i, j, length;
+
+	if (ac == 0 || av == NULL)
 	{
-		if ((str[i] != ' ' || *str != '\t') &&
-				((str[i + 1] == ' ' || str[i + 1] == '\t') || str[i + 1] == '\n'))
-			count++;
+		return (NULL);
 	}
-	if (count == 0)
-		return (NULL);
-	array = malloc(sizeof(char *) * (count + 1));
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0' && k < count; i++)
+
+	for (i = 0, length = 0; i < ac; i++)
 	{
-		if (str[i] != ' ' || str[i] != '\t')
+		for (j = 0; av[i][j] != '\0'; j++, length++)
+			;
+		length++;
+	}
+	length++;
+
+	x = malloc(length * sizeof(char));
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+
+	newstr = x;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			len = 0;
-			j = i;
-			while ((str[j] != ' ' || str[j] != '\t') && str[j] != '\0')
-				j++, len++;
-			array[k] = malloc((len + 1) * sizeof(char));
-			if (array[k] == NULL)
-			{
-				for (k = k - 1; k >= 0; k++)
-					free(array[k]);
-				free(array);
-				return (NULL);
-			}
-			for (m = 0; m < len; m++, i++)
-				array[k][m] = str[i];
-			array[k++][m] = '\0';
+			*x = av[i][j];
+			x++;
 		}
+		*x = '\n';
+		x++;
 	}
-	array[k] = NULL;
-	return (array);
+
+	return (newstr);
 }
