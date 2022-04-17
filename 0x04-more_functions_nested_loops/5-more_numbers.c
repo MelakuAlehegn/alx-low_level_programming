85bff9e152a768311faf99e5a932209b39eea7ac
melakualehgn <melakualehegn34@gmail.com>
2022-04-17 05:27:11 -0700

    5-more_numbers : fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,18 @@ void more_numbers(void)
 	int i = 0;
 	int j = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		while (j <= 14)
 		{
 			if (j > 9)
 			{
-			_putchar(j / 10);
+				_putchar(j / 10 + '0');
 			}
 			j++;
+			_putchar(j % 10);
 		}
-		_putchar(j % 10);
-		_putchar(10);
-		j++;
+		_putchar('\n');
+		i++;
 	}
 }
