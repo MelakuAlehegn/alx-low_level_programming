ca413e90bf483c4b0c0d6724beb2410abf1f41fd
melakualehgn <melakualehegn34@gmail.com>
2022-07-02 01:03:49 -0700

    hash-betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,6 +5,7 @@
  * @size: size of the array of the hash table
  * Return: Pointer to the newly created hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash;
