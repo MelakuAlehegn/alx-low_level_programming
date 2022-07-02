2621a99f649459fa719709aa4d827088945bd7c7
melakualehgn <melakualehegn34@gmail.com>
2022-07-02 02:52:13 -0700

    betty fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
-/*
+
+/**
  * key_index - a function to return the index of a key
  * @key: key
  * @size: size of hash array
@@ -8,5 +9,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return(hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
