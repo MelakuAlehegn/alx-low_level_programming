902d41e7374aaade089fcb9ea575a5546a83ac7e
melakualehgn <melakualehegn34@gmail.com>
2022-07-02 02:46:54 -0700

    key->index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/*
+ * key_index - a function to return the index of a key
+ * @key: key
+ * @size: size of hash array
+ * Return: the index in a key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return(hash_djb2(key) % size);
+}
