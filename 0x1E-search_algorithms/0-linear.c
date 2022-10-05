7818b523791068e6d4e2b480ef50e1fd01ff8853
melakualehegn <melakualehegn34@gmail.com>
2022-10-05 22:53:27 +0300

    Task-1:fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,22 +9,18 @@
  *          return -1
  */
 
-
 int linear_search(int *array, size_t size, int value)
 {
-if (array == NULL)
-{
-return (-1);
-}
-
 size_t a;
+
+if (array == NULL)
+return (-1);
+
 for (a = 0; a < size; a++)
 {
 printf("Value checked array[%ld] = [%d]\n", a, array[a]);
 if (array[a] == value)
-{
-return (array[a]);
-}
+return (a);
 }
 return (-1);
 }
