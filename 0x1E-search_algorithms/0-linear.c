cfa9c5ea48f6a56afbc802e5f98fef07c43eed75
melakualehegn <melakualehegn34@gmail.com>
2022-10-05 22:19:22 +0300

    Task:1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ *  linear_search - serach a value in a given array and return the index
+ *  @array: is a poiner to the first element of an serach
+ *  @size: the number of elements in the array
+ *  @value: the value to serach for
+ *  Return: if the value is not present of the give array is null
+ *          return -1
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+if (array == NULL)
+{
+return (-1);
+}
+
+size_t a;
+for (a = 0; a < size; a++)
+{
+printf("Value checked array[%ld] = [%d]\n", a, array[a]);
+if (array[a] == value)
+{
+return (array[a]);
+}
+}
+return (-1);
+}
